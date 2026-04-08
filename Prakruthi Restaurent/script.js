c0e7515b55b5925ffc4ca1b49c0a6a136548a9c4
SHARATH KUMAR GURUMURTHY <sharusarath793@gmail.com>
2026-04-08 21:56:04 +0530

    Add files via upload

---
+++ Prakruthi Restaurent/script.js
@@ -0,0 +1,7 @@
+// Auto update year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Mobile menu toggle
+function toggleMenu(){
+  document.getElementById("nav").classList.toggle("active");
+}
